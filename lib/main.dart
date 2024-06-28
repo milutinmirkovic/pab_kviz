1ca61d02d72c7585a62320efdd1ab1aa1cd228ed
rkomi001 <djordjemirkovic001@gmail.com>
2024-06-28 18:07:27 +0200

    Drugaciji pristup loginu sa authtenticate stranicom

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
  import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+import 'package:pab_kviz/wrapper.dart';
+//import 'pages/login_page.dart';
 
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MaterialApp( 
-      debugShowCheckedModeBanner: false, 
-      initialRoute: '/', 
-      routes: {'/': (context) => const Login()}, 
-));
+  runApp(MyApp());
 }
 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Wrapper(),
+    );
+  }
+}
