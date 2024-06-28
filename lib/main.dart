c89c24ec1f63fb041e914b0793d1ed0e249984dd
rkomi001 <djordjemirkovic001@gmail.com>
2024-06-28 13:36:40 +0200

    Proba

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Luti'),
+      home: const MyHomePage(title: 'Flutter Demo Home DJOLE I LUTI'),
     );
   }
 }
